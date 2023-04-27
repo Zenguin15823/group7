c321532c92557921d1a57ece21ae1682140b7464
Zenguin15823 <90000508+Zenguin15823@users.noreply.github.com>
2023-04-27 16:29:46 -0500

    Incorporated User Input and Win/Tie Checking
    I made some changes to the win/tie checking code just to get it all working. As of now, everything runs great as far as I can tell. Just need Computer input and then it should be good to  go.

--- project.asm
+++ project.asm
@@ -6,16 +6,22 @@ grid: .word 32, 32, 32, 32, 32, 32, 32, 32, 32 # grid is an array of 9 words, ea
 
 matrix: .word 48, 49, 50, 51, 52, 53, 54, 55, 56
 bigLine: .asciiz "-+-+-\n"
-welcome: .asciiz "\nWelcome to Tic Tac Toe!\n"
-howto: .asciiz "\nAbove, the numbers corresponding to each cell are shown. Please input a number to draw an X in that square.\n"
+welcome: .asciiz "\nWelcome to Tic Tac Toe! Here are the rules: \nYou will make a move, and then the computer will make one, then repeat until someone gets 3 of their letter in a row!"
+howto: .asciiz "\nAbove, the numbers corresponding to each cell are shown."
+howtwo: .asciiz "\nPlease input a number to draw an X in that square.\n"
 playagain: .asciiz "\nEnter 0 to quit, or any other number to play again.\n"
+invalid_input_msg: .asciiz "\nInvalid input. Please choose an empty cell by entering the corresponding number.\n"
+tieGameStr: .asciiz "\nThe game was a tie!\n"
+playerWinStr: .asciiz "\nCongratulations, you won!.\n"
+computerWinStr: .asciiz "\nThe computer won!\n"
+playAgainStr: .asciiz "\nPlay again?\n"
 
 .text
 j main
 
-# Draw board ( Courtney )
+# Draw grid ( Courtney )
 drawBoard:
-	# draws an example board if s0 is 0, otherwise draws the current board
+	# draws an example grid if s0 is 0, otherwise draws the current grid
 
 	# new line
 	li $a0, 10
@@ -32,7 +38,7 @@ loopRows:
 	#column counter
 	li $t5, 0 
 	
-	loopCols:	
+loopCols:
     	
 	#load elemnet matrix
     	lw $a0, 0($t1)
@@ -68,13 +74,103 @@ loopRows:
 	addi $t0, $t0, 1
 	blt $t0, 3, loopRows
 	
-jr $ra # end of drawBoard function
+	jr $ra # end of drawBoard function
 
-main:
+# Check for winner or tie, if no go back to gameLoop, if so go ahead ( Austin )
+# Output win/tie message ( Austin )
+checkWin:
+	li $s0, 264 # 88 x 3 = 264. If three cells sum to this number, there are 3 X's
+	li $s1, 237 # 79 x 3 = 237. If three cells sum to this number, there are 3 O's
+
+	la $t1, grid
+	li $t5, 2 # iterator so we only loop 3 times
+	checkRow:
+	lw $t2, 0($t1)
+	lw $t3, 4($t1)
+	lw $t4, 8($t1) 
+	add $t0, $t2, $t3 
+	add $t0, $t0, $t4 # add up each cell in the row,
+	beq $t0, $s0, playerWin # then check if there are 3 X's
+	beq $t0, $s1, computerWin # or 3 O's
+	beqz $t5, checkRowOut # if all 3 rows have been checked, exit
+	subi $t5, $t5, 1
+	addi $t1, $t1, 12 # otherwise, iterate to next row and go again
+	j checkRow
+	checkRowOut:
+	
+	la $t1, grid
+	li $t5, 2 # iterator so we only loop 3 times
+	checkCol:
+	lw $t2, 0($t1)
+	lw $t3, 12($t1)
+	lw $t4, 24($t1)
+	add $t0, $t2, $t3
+	add $t0, $t0, $t4 # add up each cell in the column,
+	beq $t0, $s0, playerWin # then check if there are 3 X's
+	beq $t0, $s1, computerWin # or 3 O's
+	beqz $t5, checkColOut # if all 3 columns have been checked, exit
+	subi $t5, $t5, 1
+	addi $t1, $t1, 4 # otherwise, iterate to next column and go again
+	j checkCol
+	checkColOut:
+	
+	la $t1, grid
+	# Now let's check the top left to bottom right diagonal
+	lw $t2, 0($t1)
+	lw $t3, 16($t1)
+	lw $t4, 32($t1)
+	add $t0, $t2, $t3
+	add $t0, $t0, $t4 # add up each cell in the diagonal,
+	beq $t0, $s0, playerWin # then check if there are 3 X's
+	beq $t0, $s1, computerWin # or 3 O's
+
+	# And the the top right to bottom left diagonal
+	lw $t2, 8($t1)
+	lw $t3, 16($t1)
+	lw $t4, 24($t1)
+	add $t0, $t2, $t3
+	add $t0, $t0, $t4 # add up each cell in the diagonal,
+	beq $t0, $s0, playerWin # then check if there are 3 X's
+	beq $t0, $s1, computerWin # or 3 O's
+	
+	# Finally, let's check for a tie by whether or not any blank spaces remain
+	li $t5, 9 # iterator
+	li $s2, 32 # ascii value of space, if we find one then there are still possible moves and thus not a tie
+	checkTie:
+	lw $t0, 0($t1)
+	beq $t0, $s2, noTie # not a tie if there is a blank cell, exit loop
+	subi $t5, $t5, 1 # iterate
+	addi $t1, $t1, 4 # iterate to next cell
+	bnez $t5, checkTie # if there are still cells to check, loop
+	j tieGame # if no cells remain and none were blank, it is a tie.
+
+	noTie:
+	
+	jr $ra
+	
+playerWin:
+la $a0, playerWinStr
+li $v0, 4
+syscall
+j endScr
+
+computerWin:
+la $a0, computerWinStr
+li $v0, 4
+syscall
+j endScr
+
+tieGame:
+la $a0, tieGameStr
+li $v0, 4
+syscall
+j endScr
+
+main: # this is where the fun begins
 
 # game introduction and start point ( Zac )
 
-# Clear the board in case 'play again' was chosen
+# Clear the grid in case 'play again' was chosen
 li $t0, 32
 la $t1, grid
 li $t2, 9
@@ -94,22 +190,49 @@ la $a0, howto
 li $v0, 4
 syscall
 
+gameLoop:
 
 # User input ( Berlin )
+user_input:
+    la $a0, howtwo
+    li $v0, 4
+    syscall
+    li $v0, 5                   # syscall for reading an integer
+    syscall
+    sll $t0, $v0, 2             # multiply the index by 4 to get the correct offset in the array
+    la $t1, grid                # load the address of the grid array
+    add $t1, $t1, $t0           # add the offset to the grid address
+    lw $t2, 0($t1)              # load the value at the chosen position
+    li $t3, 32                  # load the value 32 (ASCII space)
+    bne $t2, $t3, invalid_input # if the chosen position is not empty, input is invalid
+
+    li $t4, 88                  # load the value 88 (ASCII 'X')
+    sw $t4, 0($t1)              # store the value 'X' in the chosen position
+    j end_user_input            # jump to the end of the user input section
+
+invalid_input:
+    # Output an error message and repeat the user input process
+    la $a0, invalid_input_msg
+    li $v0, 4
+    syscall
+    j user_input
+
+end_user_input:
 
 li $s0, 1 # These two lines will call the drawBoard function so the player can see the X's and O's
 jal drawBoard 
+jal checkWin # This line checks for a win/tie, and if one is found it ends the game accordingly.
 
 # Computer input ( Morgod )
 
 li $s0, 1 # These two lines will call the drawBoard function so the player can see the X's and O's
 jal drawBoard 
 
-# Check for winner or tie, if no go back to gameLoop, if so go ahead ( Austin )
 
-# Output win/tie message ( Austin )
-
+jal checkWin # This line checks for a win/tie, and if one is found it ends the game accordingly.
+j gameLoop # If no win or tie was found, the game continues
 # Give option to play again/quit ( Zac )
+endScr:
 
 la $a0, playagain
 li $v0, 4
@@ -121,5 +244,7 @@ bnez $v0, main
 # When you are done with your part, commit it to the gitHub as its own branch and I will put it into the master branch. 
 # If something is wrong with it I will try to debug but might need your help ( Zac )
 # If you have any questions or anything just msg the discord chat
+
+exit:
 li $v0, 10
 syscall
